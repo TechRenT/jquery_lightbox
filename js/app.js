8df9a60714f2bba12a1eac2c94c931081cff0542
Renelle <titingrenelle@gmail.com>
2017-03-12 07:04:15 +0800

    perform - part 3

--- js/app.js
+++ js/app.js
@@ -9,29 +9,30 @@
 //A caption to overlay
 
 var $overlay = $('<div id="overlay"></div>');
+var $image = $('<img>');
 //Add overlay
 $("body").append($overlay);
+$overlay.append($image)
 
 
 //Capture the click event on a link to an image
 $("#imageGallery a").click(function(event) {
   event.preventDefault();
-  var href = $(this).attr("href");
+  var imageLocation = $(this).attr("href");
+  //Update overlay with the image linked in the link
+  $("#overlay img").attr("src", imageLocation)
+  //Show the overlay.
   $overlay.show();
 });
-
-  //Update overlay with the image linked in the link
-  
-  
-  //Show the overlay.
-  
   
   //Get child's alt attribute and set caption
   
 
 //When overlay is clicked
-
+$overlay.click(function(){
   //Hide the overlay
+  $overlay.hide();
+})
   
 
 
