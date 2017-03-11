185124a11a233a555f72f45211c8a30c7dae77cb
Renelle <titingrenelle@gmail.com>
2017-03-11 16:46:24 +0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,39 @@
+//Problem: User when clicking on image goes to a dead end
+//Solution: Create an overlay with the large image - Lightbox
+
+
+
+//An image to overlay
+
+
+//A caption to overlay
+
+
+//Add overlay
+
+
+//Capture the click event on a link to an image
+
+  //Update overlay with the image linked in the link
+  
+  
+  //Show the overlay.
+  
+  
+  //Get child's alt attribute and set caption
+  
+
+//When overlay is clicked
+
+  //Hide the overlay
+  
+
+
+
+
+
+
+
+
+
+
